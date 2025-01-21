6c2732ef0b5ff6c4199d3cb6974048a29ed3ad23
Nada El-Falou <nadafalou2001@gmail.com>
2025-01-21 11:16:09 -0800

    minor changes and corrections

---
+++ py_files/brick_ringmaps.py
@@ -0,0 +1,159 @@
+import numpy as np
+from astrometry.util.fits import *
+from astropy.io import fits
+from scipy.interpolate import RectBivariateSpline
+import sys
+
+
+BINNING = 16
+BY = int(4096 // BINNING)
+BX = int(2048 // BINNING)
+
+
+def get_gaia_data(directory='/pscratch/sd/d/dstn/forced-motions-3/'):
+    G = merge_tables([
+            fits_table(directory + '/gaia-1k.fits'),
+            fits_table(directory + '/gaia-1k-to-4k.fits'),
+            fits_table(directory + '/gaia-4k-to-end.fits'),
+        ])
+    return G
+
+
+def make_ringmaps(data, filename):
+    ringmaps = {}
+    ccdnames = np.unique(data.ccdname)
+    first = True
+
+    for ccdname in ccdnames:
+        for filt in ['g','r','i','z']:
+            J = np.flatnonzero((data.ccdname == ccdname)
+                                * np.isin(data.filter, filt)
+                                * (data.full_fit_dra_ivar > 1)
+                                * (data.dcr_full_fit_dra != 0.)
+                                * (data.dqmask == 0))
+            # number of pixels to bin together for tree-rings
+            xbin = np.floor(data.x[J] / BINNING).astype(int)
+            ybin = np.floor(data.y[J] / BINNING).astype(int)
+            dxvals = {}
+            dyvals = {}
+            for xb,yb,dx,dy in zip(xbin, ybin, data.dcr_full_fit_x[J] - data.x[J], data.dcr_full_fit_y[J] - data.y[J]):
+                if not (xb,yb) in dxvals:
+                    dxvals[(xb,yb)] = []
+                    dyvals[(xb,yb)] = []
+                dxvals[(xb,yb)].append(dx)
+                dyvals[(xb,yb)].append(dy)
+            dxmap = np.zeros((BY,BX), np.float32)
+            dymap = np.zeros((BY,BX), np.float32)
+            for yb in range(BY):
+                for xb in range(BX):
+                    if not (xb,yb) in dxvals:
+                        continue
+                    v = dxvals[(xb,yb)]
+                    dxmap[yb,xb] = np.median(v)
+                    v = dyvals[(xb,yb)]
+                    dymap[yb,xb] = np.median(v)
+            ringmaps[(ccdname, filt)] = (dxmap, dymap)
+
+            img = np.dstack((dxmap,dymap))
+            extname = '%s-%s' % (ccdname, filt)
+            fitsio.write(filename, img, clobber=first, extname=extname)
+            first = False
+
+            
+def get_ringmaps(filename):
+    ringmaps = fits.open(filename)
+    ringmaps_dict = {}
+
+    for rmap in ringmaps:
+        ccdname, filt = rmap.name[:-2], rmap.name[-1:]
+        ringmaps_dict[(ccdname, filt)] = (rmap.data[:,:,0], rmap.data[:,:,1])
+    return ringmaps_dict
+
+
+def make_splines(ringmaps):
+    # copied and edited from https://github.com/gbernstein/pixmappy/blob/2c912e2f37d03fc0f0892768dd359d3ec7743618/pixmappy/DESMaps.py#L99C20-L99C20
+    # makes the tweak map spline thing
+    tweaks = {}
+
+    for rmap in ringmaps.keys():
+        # Locate grid points, in 1-indexed pixel system
+        xvals = BINNING * np.arange(BX) + 0.5 * BINNING + 1
+        yvals = BINNING * np.arange(BY) + 0.5 * BINNING + 1
+        bbox = [1, BX * BINNING + 1, 1, BY * BINNING + 1]
+        # Create linear spline for x and y components
+        # Note that data array comes in with (y,x) indexing
+        tweaks[rmap] = (RectBivariateSpline(xvals, yvals, ringmaps[rmap][0].transpose(),
+                                                    bbox=bbox, kx=1, ky=1),
+                            RectBivariateSpline(xvals, yvals, ringmaps[rmap][1].transpose(),
+                                                    bbox=bbox, kx=1, ky=1))
+    return tweaks
+
+
+def create_brick_corrected_data(filename, corr_dir, old_dir, tweaks):
+    f = os.path.join(old_dir, filename)
+    forced_table = fits.open(f)
+    corr_table = forced_table.copy()
+    ccdnames = np.unique(corr_table[1].data.ccdname)
+
+    corr_table[1].columns.add_col(fits.Column(name="rm_full_fit_x", array=np.zeros(len(corr_table[1].data)), format='E', unit='pixel'))
+    corr_table[1].data["rm_full_fit_x"] = (corr_table[1].data.dcr_full_fit_x).copy()
+    corr_table[1].columns.add_col(fits.Column(name="rm_full_fit_y", array=np.zeros(len(corr_table[1].data)), format='E', unit='pixel'))
+    corr_table[1].data["rm_full_fit_y"] = (corr_table[1].data.dcr_full_fit_y).copy()
+    corr_table[1].columns.add_col(fits.Column(name="rm_full_fit_dra", array=np.zeros(len(corr_table[1].data)), format='E', unit='arcsec'))
+    corr_table[1].data["rm_full_fit_dra"] = (corr_table[1].data.dcr_full_fit_dra).copy()
+    corr_table[1].columns.add_col(fits.Column(name="rm_full_fit_ddec", array=np.zeros(len(corr_table[1].data)), format='E', unit='arcsec'))
+    corr_table[1].data["rm_full_fit_ddec"] = (corr_table[1].data.dcr_full_fit_ddec).copy()
+
+    for ccdname in ccdnames:
+        for filt in ['g','r','i','z']:
+            J = np.flatnonzero((corr_table[1].data.ccdname == ccdname)
+                                * np.isin(corr_table[1].data.filter, filt)
+                                * (corr_table[1].data.full_fit_dra_ivar > 1e4)
+                                * (corr_table[1].data.full_fit_dra != 0.)
+                                * (corr_table[1].data.dqmask == 0))
+
+            spline = tweaks[(ccdname, filt)]
+            xpos = corr_table[1].data.dcr_full_fit_x[J]
+            ypos = corr_table[1].data.dcr_full_fit_y[J]
+
+            xpos, ypos = xpos - spline[0](xpos, ypos, grid=False), ypos - spline[1](xpos, ypos, grid=False)
+
+            dDEC = -0.262 * (xpos - corr_table[1].data.dcr_full_fit_x[J]) # bc Dustin said so
+            dRA = +0.262 * (ypos - corr_table[1].data.dcr_full_fit_y[J])
+
+            corr_table[1].data["rm_full_fit_x"][J] = xpos
+            corr_table[1].data["rm_full_fit_y"][J] = ypos
+            corr_table[1].data["rm_full_fit_dra"][J] = corr_table[1].data["dcr_full_fit_dra"][J] + dRA
+            corr_table[1].data["rm_full_fit_ddec"][J] = corr_table[1].data["dcr_full_fit_ddec"][J] + dDEC
+    corr_table.writeto(f"{ corr_dir }{ filename }", overwrite=True)
+
+    
+def create_corrected_data(corr_dir, old_dir, tweaks, cont=False):
+    for filename in os.listdir(old_dir):
+        f = os.path.join(old_dir, filename)
+        if not os.path.isfile(f) or filename[:6] != 'forced' or filename[:4] != 'gaia' or (cont is True and filename in os.listdir(corr_dir)):
+            continue
+        print(f)
+        
+        create_brick_corrected_data(filename, corr_dir, old_dir, tweaks)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("NO")
+    filename, corr_dir, old_dir = sys.argv[1:]
+    rmaps = get_ringmaps('/global/cfs/cdirs/cosmo/work/users/nelfalou/ls-motions/rm-corrected-forced-motions/' + 'ringmaps.fits')
+    tweaks = make_splines(rmaps)
+    create_brick_corrected_data(filename, corr_dir, old_dir, tweaks)
+    
+# def main():  
+#     corr_dir = '../../../cfs/cdirs/cosmo/work/users/nelfalou/ls-motions/rm-corrected-forced-motions/'
+#     old_dir = '../../../cfs/cdirs/cosmo/work/users/nelfalou/ls-motions/dcr-corrected-forced-motions/'
+
+#     fn = 'ringmaps.fits'
+#     G = ringmaps.get_gaia_data(old_dir)
+#     ringmaps.make_ringmaps(G, corr_dir + fn)
+#     rmaps = ringmaps.get_ringmaps(corr_dir + fn)
+#     tweaks = ringmaps.make_splines(rmaps)
+
+#     ringmaps.create_corrected_data(corr_dir, old_dir, tweaks, cont=True)
---
+++ py_files/create_brick_catalogue.py
@@ -0,0 +1,201 @@
+from astropy.io import fits
+import numpy as np
+from astropy.time import Time
+from astropy.table import Table, vstack
+from astropy import units as u
+from astrometry.util.fits import os, fits_table
+from legacypipe.survey import radec_at_mjd, mjd_to_year
+import sys
+
+DAYSPERYEAR = 365.2425
+
+    
+def pm_lsq_plx(N, dt, plx_dra, plx_ddec, dra_error, ddec_error, obj_filt, B):
+    A = np.zeros((2 * N, 5))
+    A[:N, 0] = plx_dra / dra_error[obj_filt] 
+    A[N:, 0] = plx_ddec / ddec_error[obj_filt]
+    A[:N, 1] = dt / dra_error[obj_filt]
+    A[N:, 2] = dt / ddec_error[obj_filt]
+    A[:N, 3] = np.ones(N)
+    A[N:, 4] = np.ones(N)
+
+    X, resid, rank, s = np.linalg.lstsq(A, B, rcond=None) # in arcsec
+    X = X * 1000 # arcsec to mas
+    # parallax, pmra_plx, pmdec_plx, ra_offset_plx, dec_offset_plx = X[0], X[1], X[2], X[3], X[4] # mas, mas/yr, mas/yr
+    # ra_offset_plx = ra_offset_plx / 1000 # to arcsec
+    # dec_offset_plx = dec_offset_plx / 1000 # to arcsec
+
+    cov_matrix = np.linalg.inv(A.T @ A)
+    variances = np.diag(cov_matrix) * 1000**2
+    # parallax_var, pmra_plx_var, pmdec_plx_var, ra_offset_plx_var, dec_offset_plx_var = variances
+                
+    return X, variances  # all in mas
+
+
+def pm_lsq(N, dt, dra_error, ddec_error, obj_filt, B):    
+    A = np.zeros((2 * N, 4))
+    A[:N, 0] = dt / dra_error[obj_filt]
+    A[N:, 1] = dt / ddec_error[obj_filt]
+    A[:N, 2] = np.ones(N)
+    A[N:, 3] = np.ones(N)
+
+    X, resid, rank, s = np.linalg.lstsq(A, B, rcond=None) # in arcsec
+    X = X * 1000
+    # pmra, pmdec, ra_offset, dec_offset = X[0], X[1], X[2] # mas, mas/yr, mas/yr
+
+    cov_matrix = np.linalg.inv(A.T @ A)
+    variances = np.diag(cov_matrix) * 1000**2
+    # pmra_var, pmdec_var, ra_offset_var, dec_offset_var = variances
+    
+    return X, variances  # all in mas
+    
+    
+def calculate_pm(forced, tractor, objid, dra_error, ddec_error, original_data=False):
+    ref_year = 0 # bc dra, ddec around 0, not actual ra, decs
+    obj_filt = forced.objid == objid
+
+    plx_ra, plx_dec = [], []
+    for date in forced.mjd[obj_filt]:
+        ra, dec = radec_at_mjd(tractor.ra[objid], tractor.dec[objid], ref_year, 0, 0, 1000, date) # mas/ye and mas, returns deg
+        plx_ra.append(ra)
+        plx_dec.append(dec)
+    plx_dra, plx_ddec = (np.array(plx_ra) - tractor.ra[objid]) * np.cos(np.deg2rad(tractor.dec[objid])) * 3600, (np.array(plx_dec) - tractor.dec[objid]) * 3600 # change in ra, dec due to parallax = 1 arcsec
+    dra_ivar = 1 / dra_error ** 2
+    ref_mjd = np.sum((dra_ivar * forced.mjd)[obj_filt]) / np.sum(dra_ivar[obj_filt]) # ref_mjd for this dataset is different from Gaia's ref_mjd
+                                                                                     # theoretically, dra_ivar ~= ddec_ivar
+    dt = (forced.mjd[obj_filt] - ref_mjd) / DAYSPERYEAR
+    N = len(forced.full_fit_dra[obj_filt])
+    if original_data:
+        B = np.append((forced.full_fit_dra / dra_error)[obj_filt], (forced.full_fit_ddec / ddec_error)[obj_filt])
+    else:
+        B = np.append((forced.lm_full_fit_dra / dra_error)[obj_filt], (forced.lm_full_fit_ddec / ddec_error)[obj_filt])
+    
+    try:
+        X_plx, variances_plx = pm_lsq_plx(N, dt, plx_dra, plx_ddec, dra_error, ddec_error, obj_filt, B)
+    except np.linalg.LinAlgError as err:
+        X_plx, variances_plx = None, None
+
+    try:
+        X, variances = pm_lsq(N, dt, dra_error, ddec_error, obj_filt, B)
+    except np.linalg.LinAlgError as err:
+        X, variances = None, None
+        
+    return X_plx, variances_plx, X, variances, ref_mjd        
+    
+    
+def create_brick_catalogue(filename, new_dir, old_dir, tractor_dir, original_data=False):
+    f = os.path.join(old_dir, filename)
+    forced_table = fits.open(f)
+    tractor_table = fits.open(tractor_dir + "tractor-forced-" + f[-13:])
+    pm_table = tractor_table.copy()
+
+    pmra_plx = np.zeros(len(tractor_table[1].data))
+    pmra_plx_ivar = np.zeros(len(tractor_table[1].data))
+    pmdec_plx = np.zeros(len(tractor_table[1].data))
+    pmdec_plx_ivar = np.zeros(len(tractor_table[1].data))
+    parallax = np.zeros(len(tractor_table[1].data))
+    parallax_ivar = np.zeros(len(tractor_table[1].data))
+    new_ra_plx = np.zeros(len(tractor_table[1].data))
+    new_ra_plx_ivar = np.zeros(len(tractor_table[1].data))
+    new_dec_plx = np.zeros(len(tractor_table[1].data))
+    new_dec_plx_ivar = np.zeros(len(tractor_table[1].data))
+    pmra = np.zeros(len(tractor_table[1].data))
+    pmra_ivar = np.zeros(len(tractor_table[1].data))
+    pmdec = np.zeros(len(tractor_table[1].data))
+    pmdec_ivar = np.zeros(len(tractor_table[1].data))
+    new_ra = np.zeros(len(tractor_table[1].data))
+    new_ra_ivar = np.zeros(len(tractor_table[1].data))
+    new_dec = np.zeros(len(tractor_table[1].data))
+    new_dec_ivar = np.zeros(len(tractor_table[1].data))
+    pm_flag = np.zeros(len(tractor_table[1].data))
+    ref_mjd = np.zeros(len(tractor_table[1].data))
+
+    dra_ivar = forced_table[1].data.full_fit_dra_ivar
+    ddec_ivar = forced_table[1].data.full_fit_ddec_ivar
+    dra_error = 1 / np.sqrt(dra_ivar)
+    ddec_error = 1 / np.sqrt(ddec_ivar)
+    dra_error = np.hypot(dra_error, 0.014806002)
+    ddec_error = np.hypot(ddec_error, 0.014806002)
+    # dra_ivar = 1 / dra_error ** 2 
+    # ddec_ivar = 1 / ddec_error ** 2 
+
+    J = np.flatnonzero(tractor_table[1].data.type == 'PSF')
+    pm_flag[~J] = -1
+
+    fails = []
+
+    for i, objid in zip(J, tractor_table[1].data.objid[J]):
+        X_plx, variances_plx, X, variances, rmjd = calculate_pm(forced_table[1].data, tractor_table[1].data, objid, dra_error, ddec_error, original_data)
+        if X_plx is None:
+            pm_flag[i] = 0
+            dat = forced_table[1].data.full_fit_dra[forced_table[1].data.objid == objid]
+            if dat.size != 0 and sum(dat) != 0:
+                fails.append(objid)
+        else:
+            parallax[i], pmra_plx[i], pmdec_plx[i], ra_offset, dec_offset = X_plx # in mas, mas/yr
+            new_ra_plx[i] = tractor_table[1].data.ra[objid] + ra_offset / 1000 / np.cos(np.deg2rad(tractor_table[1].data.dec[objid])) / 3600
+            new_dec_plx[i] = tractor_table[1].data.dec[objid] + dec_offset / 1000 / 3600
+            parallax_ivar[i], pmra_plx_ivar[i], pmdec_plx_ivar[i], ra_offset_ivar, dec_offset_ivar = 1 / variances_plx
+            new_ra_plx_ivar[i] = 1 / ((1 / np.sqrt(ra_offset_ivar)) / 1000 / np.cos(np.deg2rad(tractor_table[1].data.dec[objid])) / 3600)**2
+            new_dec_plx_ivar[i] = 1 / ((1 / np.sqrt(dec_offset_ivar)) / 1000 / 3600)
+
+        if X is None:
+            pm_flag[i] = 0
+        else:
+            pmra[i], pmdec[i], ra_offset, dec_offset = X # in mas, mas/yr
+            new_ra[i] = tractor_table[1].data.ra[objid] + ra_offset / 1000 / np.cos(np.deg2rad(tractor_table[1].data.dec[objid])) / 3600
+            new_dec[i] = tractor_table[1].data.dec[objid] + dec_offset / 1000 / 3600
+            pmra_ivar[i], pmdec_ivar[i], ra_offset_ivar, dec_offset_ivar = 1 / variances
+            new_ra_ivar[i] = 1 / ((1 / np.sqrt(ra_offset_ivar)) / 1000 / np.cos(np.deg2rad(tractor_table[1].data.dec[objid])) / 3600)**2
+            new_dec_ivar[i] = 1 / ((1 / np.sqrt(dec_offset_ivar)) / 1000 / 3600)
+
+        ref_mjd[i] = rmjd
+        pm_flag[i] = 1  
+
+    pm_table[1].columns.add_col(fits.Column(name="pmra_plx", array=pmra_plx, format='E', unit='mas/yr'))
+    pm_table[1].columns.add_col(fits.Column(name="pmra_plx_ivar", array=pmra_plx_ivar, format='E', unit='(mas/yr)^(-2)'))
+    pm_table[1].columns.add_col(fits.Column(name="pmdec_plx", array=pmdec_plx, format='E', unit='mas/yr'))
+    pm_table[1].columns.add_col(fits.Column(name="pmdec_plx_ivar", array=pmdec_plx_ivar, format='E', unit='(mas/yr)^(-2)'))
+    pm_table[1].columns.add_col(fits.Column(name="parallax_new", array=parallax, format='E', unit='mas'))
+    pm_table[1].columns.add_col(fits.Column(name="parallax_new_ivar", array=parallax_ivar, format='E', unit='mas^(-2)'))
+    pm_table[1].columns.add_col(fits.Column(name="new_ra_plx", array=new_ra_plx, format='E', unit='deg'))
+    pm_table[1].columns.add_col(fits.Column(name="new_dec_plx", array=new_dec_plx, format='E', unit='deg'))
+    pm_table[1].columns.add_col(fits.Column(name="new_ra_plx_ivar", array=new_ra_plx_ivar, format='E', unit='deg^(-2)'))
+    pm_table[1].columns.add_col(fits.Column(name="new_dec_plx_ivar", array=new_dec_plx_ivar, format='E', unit='deg^(-2)'))
+    pm_table[1].columns.add_col(fits.Column(name="pmra_no_plx", array=pmra, format='E', unit='mas/yr'))
+    pm_table[1].columns.add_col(fits.Column(name="pmra_no_plx_ivar", array=pmra_ivar, format='E', unit='(mas/yr)^(-2)'))
+    pm_table[1].columns.add_col(fits.Column(name="pmdec_no_plx", array=pmdec, format='E', unit='mas/yr'))
+    pm_table[1].columns.add_col(fits.Column(name="pmdec_no_plx_ivar", array=pmdec_ivar, format='E', unit='(mas/yr)^(-2)'))
+    pm_table[1].columns.add_col(fits.Column(name="new_ra_no_plx", array=new_ra, format='E', unit='deg'))
+    pm_table[1].columns.add_col(fits.Column(name="new_dec_no_plx", array=new_dec, format='E', unit='deg'))
+    pm_table[1].columns.add_col(fits.Column(name="new_ra_no_plx_ivar", array=new_ra_ivar, format='E', unit='deg^(-2)'))
+    pm_table[1].columns.add_col(fits.Column(name="new_dec_no_plx_ivar", array=new_dec_ivar, format='E', unit='deg^(-2)'))
+    pm_table[1].columns.add_col(fits.Column(name="pm_flag", array=pm_flag, format='E'))
+    pm_table[1].columns.add_col(fits.Column(name="ref_mjd", array=ref_mjd, format='E'))
+
+    print("done! now writing to file...")
+
+    if original_data:
+        pm_table.writeto(f"{ new_dir }tractor-original-pm-{ filename[-13:] }", overwrite=True)
+    else:
+        pm_table.writeto(f"{ new_dir }tractor-pm-{ filename[-13:] }", overwrite=True)
+    return fails
+    
+    
+def create_catalogues(new_dir, old_dir, tractor_dir, original_data=False, cont=True):
+    fails = []
+    for filename in os.listdir(old_dir):
+        f = os.path.join(old_dir, filename)
+        if not os.path.isfile(f) or filename[:6] != 'forced' or (cont is True and filename in os.listdir(new_dir)):
+            continue
+        print(filename[-13:])
+        
+        fails.extend(create_brick_catalogue(filename, new_dir, old_dir, tractor_dir, original_data))
+    return fails
+
+    
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print(sys.argv)
+    filename, new_dir, old_dir, tractor_dir = sys.argv[1:]
+    create_brick_catalogue(filename, new_dir, old_dir, tractor_dir)
